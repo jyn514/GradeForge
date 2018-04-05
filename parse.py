d76b5bd337b0611bd016e6f872e1bad2a45ca50e
Joshua Nelson <jyn514@gmail.com>
2018-04-05 11:16:58 -0400

    initial commit

---
+++ parse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+# note: only works with python2 (sorry)
+from lxml import etree
+import cloudpickle
+
+stdout = 'output.data'
+document = 'USC_all_courses.html'
+
+def fast_iter(context):
+    forms = (elem.findall('input') for event, elem in context
+             if event == 'end' and elem.tag == 'form' and elem.text != 'Search')
+    return [i.items() for inputs in forms
+            for i in inputs if i.items()[1][1] != 'dummy']
+
+
+def load():
+    return cloudpickle.load(open(stdout, 'rb'))
+
+
+def save(obj):
+    with open(stdout, 'wb') as out:
+        cloudpickle.dump(obj, out)
+
+
+def main():
+    with open(document) as stdin:
+        return fast_iter(etree.iterparse(stdin, html=True))
+
+
+if __name__ == '__main__':
+    save(main())
