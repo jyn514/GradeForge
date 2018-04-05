a45ee58913671ac9e3dffb8c88eb6e6551ecb4a9
Joshua Nelson <jyn514@gmail.com>
2018-04-05 14:57:34 -0400

    make it organized; add CLI

--- parse.py
+++ parse.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python2
 from __future__ import print_function, generators
+from argparse import ArgumentParser
+from sys import argv
+
 # note: only works with python2 (sorry)
-from lxml import etree
+from lxml.etree import iterparse
 import cloudpickle
 
 stdout = 'output.data'
@@ -10,23 +13,35 @@ document = 'USC_all_courses.html'
 def fast_iter(context):
     forms = (elem.findall('input') for event, elem in context
              if event == 'end' and elem.tag == 'form' and elem.text != 'Search')
-    return [[i.items() for i in inputs]
-            for inputs in forms]
+    return [[{item[0]: item[1] for item in field.items()
+              if item[1] != 'hidden' and item[0] != 'style'}
+             for field in form if field.items()[1][1] != 'dummy']
+            for form in forms]
 
 
-def load():
-    return cloudpickle.load(open(stdout, 'rb'))
-
-
-def save(obj):
-    with open(stdout, 'wb') as out:
-        cloudpickle.dump(obj, out)
-
-
-def main():
+def main(args):
     with open(document) as stdin:
-        return fast_iter(etree.iterparse(stdin, html=True))
+        result = fast_iter(iterparse(stdin, html=True))
+    if args.load:
+        with open(args.output, 'rb') as i:
+            print(cloudpickle.load(i))
+    else:
+        with open(args.output, 'wb') as out:
+            cloudpickle.dump(result, out)
+        if args.verbose:
+            print(result)
 
 
 if __name__ == '__main__':
-    save(main())
+    parser = ArgumentParser()
+    action = parser.add_mutually_exclusive_group()
+    action.add_argument('--save', '-s', '--save-binary', action='store_true',
+                        help='Save result of main() in binary form',
+                        default=True)
+    action.add_argument('--load', '--print', '-l', action='store_true',
+                        help='Show result on stdout')
+    parser.add_argument('--verbose', '-v', help='Show result',
+                        action='store_true')
+    parser.add_argument('--output', '-o', help='File to store binary',
+                        default=stdout)
+    main(parser.parse_args())
