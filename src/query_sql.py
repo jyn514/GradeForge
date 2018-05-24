13ae1e598b71a5328cb02535454216aae1120366
Joshua Nelson <jyn514@gmail.com>
2018-05-23 21:59:32 -0400

    switch to raw SQL queries

--- src/query_sql.py
+++ src/query_sql.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from argparse import ArgumentParser
 from sqlite3 import connect
 
-from utils import arg_filter, allowed, SingleMetavarFormatter
+from utils import arg_filter, SingleMetavarFormatter
 from create_sql import TABLES
 
 DEBUG = True
@@ -24,26 +24,17 @@ def query(table='section', columns='*', **filters):
     return stdout
 
 
+def query(query, database='../classes.sql'):
+    return '\n'.join('|'.join(str(s) for s in t)
+                     for t in connect(database).execute(query).fetchall())
 
 
+def schema(database='../classes.sql'):
+    return query('SELECT sql FROM sqlite_master;', database)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(__doc__, formatter_class=SingleMetavarFormatter)
-    parser.add_argument('--semester', '-s', type=str.lower, nargs='*',
-                        choices=allowed['semester'] + ('fall', 'summer', 'spring'))
-    parser.add_argument('--campus', '-c', choices=allowed['campus'], nargs='*')
-    parser.add_argument('--number', '-n', help='course code', type=int, nargs='*')
-    parser.add_argument('--title', '-t', help='name of course', nargs='*')
-    parser.add_argument('--min_credits', '--min', '-m', type=int, nargs='?')
-    parser.add_argument('--max_credits', '--max', '-M', type=int, nargs='?')
-    parser.add_argument('--level', '-l', choices=filter_out(allowed['level']), nargs='*')
-    parser.add_argument('--term', '-T', choices=allowed['term'], nargs='*')
-    parser.add_argument('--times', choices=filter_out(allowed['times']), nargs='*')
-    parser.add_argument('--location', '-L', choices=filter_out(allowed['location']), nargs='*')
-    parser.add_argument('--department', choices=allowed['department'] + ([],),
-                        type=str.upper, nargs='*', metavar='DEPARTMENT')
-    parser.add_argument('columns', choices=tuple(TABLES.keys()) + ('*',), type=str.lower, nargs='*',
-                        metavar='COLUMNS', default='*')
+    parser.add_argument('query', default='SELECT sql FROM sqlite_master', nargs='?')
     args = arg_filter(parser.parse_args())
     print(query(**args), end='')
