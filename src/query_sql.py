3b52a9706ee37280cab1a0fcdbf19c23de4ae403
Joshua Nelson <jyn514@gmail.com>
2018-05-25 18:34:04 -0400

    refactor query_sql (again)

--- src/query_sql.py
+++ src/query_sql.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 from __future__ import print_function
-from argparse import ArgumentParser
 from sqlite3 import connect
 
-from utils import arg_filter, SingleMetavarFormatter
-from create_sql import TABLES
-
-DEBUG = True
-
-def limited_query(database='../classes.sql', table='section', columns='*', **filters):
+def limited_query(database='classes.sql', table='section', columns='*', **filters):
     '''NOTE: Does NOT validate input, that is the responsibility of calling code.
     Fails noisily if args are incorrect. Example: query_sql.py --department CSCE CSCI'''
     DATABASE = connect(database)
@@ -24,17 +18,22 @@ def limited_query(database='../classes.sql', table='section', columns='*', **fil
     return stdout
 
 
-def query(query, database='../classes.sql'):
+def query(query, database='classes.sql'):
     return '\n'.join('|'.join(str(s) for s in t)
                      for t in connect(database).execute(query).fetchall())
 
 
-def schema(database='../classes.sql'):
+def schema(database='classes.sql'):
     return query('SELECT sql FROM sqlite_master;', database)
 
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from utils import SingleMetavarFormatter
     parser = ArgumentParser(__doc__, formatter_class=SingleMetavarFormatter)
+    parser.add_argument('--database', '-d', '-f', default='classes.sql')
+    parser.add_argument('--debug', '--verbose', '-v', action='store_true')
     parser.add_argument('query', default='SELECT sql FROM sqlite_master', nargs='?')
-    args = arg_filter(parser.parse_args())
-    print(query(**args), end='')
+    args = parser.parse_args()
+    DEBUG = args.__dict__.pop('debug')
+    print(query(**args.__dict__), end='')
