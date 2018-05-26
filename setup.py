6ee1b1df30e9a4d25779caca1ec15adf29c01df8
Joshua Nelson <jyn514@gmail.com>
2018-05-26 08:15:53 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(name='gradeforge', version='0.0.1',
+      description='view available classes for the University of South Carolina',
+      author="Joshua Nelson, Brady O'Leary, Charles Daniels, and James Coman",
+      author_email='jyn514@gmail.com',
+      install_requires=['flask', 'lxml', 'requests'],
+      packages=['gradeforge', 'gradeforge.src', 'gradeforge.flask'],
+      long_description=long_description,
+      url='https://github.com/jyn514/GradeForge'  # TODO: make a website
+)
