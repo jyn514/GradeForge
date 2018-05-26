defc63758edb8c5df4482bec9c8791409810c92f
Joshua Nelson <jyn514@gmail.com>
2018-05-26 16:01:02 -0400

    add functions to init

--- gradeforge/__init__.py
+++ gradeforge/__init__.py
@@ -1,2 +1,4 @@
-from gradeforge.src import *
-from gradeforge.flask import app
+from .parse import parse_exam, parse_sections, parse_bookstore, parse_catalog
+from .download import get_exam, get_sections, get_bookstore, get_catalog
+from .utils import ReturnSame
+from .web import app
