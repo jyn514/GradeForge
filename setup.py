0e236c38f9828c1c6becbbae34da94bb091787d6
Joshua Nelson <jyn514@gmail.com>
2018-05-27 12:24:02 -0400

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,26 @@ from setuptools import setup
 with open("README.md") as f:
     long_description = f.read()
 
-setup(name='gradeforge', version='0.0.1',
+setup(name='gradeforge', version='0.0.1-dev',
       description='view available classes for the University of South Carolina',
       author="Joshua Nelson, Brady O'Leary, Charles Daniels, and James Coman",
       author_email='jyn514@gmail.com',
       install_requires=['flask', 'lxml', 'requests'],
       packages=['gradeforge', 'gradeforge.web'],
       long_description=long_description,
-      url='https://github.com/jyn514/GradeForge'  # TODO: make a website
+      url='https://github.com/jyn514/GradeForge',  # TODO: make a website
+      classifiers=(
+          'Development Status::2 - Pre-Alpha',
+          'Environment::Console',
+          'Environment::Web Environment',
+          'Framework::Flask',
+          'Intended Audience::Developers',
+          'Intended Audience::Education',
+          'Intended Audience::End Users/Desktop',
+          'Operating System::OS Independent',
+          'Programming Language::Python::3::Only',
+          'Topic::Database::Front-Ends',
+          'Topic::Internet::WWW/HTTP::Dynamic Content'
+          # TODO: add license (full list at https://pypi.org/classifiers/)
+    )
 )
