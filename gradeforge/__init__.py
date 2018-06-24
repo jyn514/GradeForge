f3a5147e5b2224e0888d15234c5b6e4195aedbd0
Joshua Nelson <jyn514@gmail.com>
2018-06-24 16:12:47 -0400

    make pylint happy

--- gradeforge/__init__.py
+++ gradeforge/__init__.py
@@ -1,5 +1,6 @@
 '''The 'API' for gradeforge, as much as python has APIs'''
-from .parse import parse_exam, parse_sections, parse_bookstore, parse_catalog, parse_grades, parse_semester
+from .parse import (parse_exam, parse_sections, parse_bookstore,
+                    parse_catalog, parse_grades, parse_semester)
 from .download import get_exam, get_sections, get_bookstore, get_catalog, get_grades
 from .sql import TABLES, create, query, dump
 from .web import app
