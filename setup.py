639360d935e49aa4c34e6c4dfaf65be5a260a57a
Joshua Nelson <jyn514@gmail.com>
2018-06-24 17:30:21 -0400

    keep requirements.txt and setup.py in sync

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ from setuptools import setup
 with open("README.md") as f:
     long_description = f.read()
 
+with open('requirements.txt') as f:
+    requirements = tuple(map(str.strip,
+                             filter(lambda s: not s.startswith('#'),
+                                    f.readlines())))
+
 setup(name='gradeforge', version='0.0.1-dev',
       description='view available classes for the University of South Carolina',
       author="Joshua Nelson, Brady O'Leary, Charles Daniels, and James Coman",
       author_email='jyn514@gmail.com',
-      install_requires=['flask', 'lxml', 'requests'],
+      install_requires=requirements,
       packages=['gradeforge', 'gradeforge.web'],
       long_description=long_description,
       url='https://github.com/jyn514/GradeForge',  # TODO: make a website
