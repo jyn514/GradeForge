283793468826222576f9586418ea4b5e49118885
Joshua Nelson <jyn514@gmail.com>
2018-06-26 18:53:09 -0400

    add ReturnSame

--- parse.py
+++ parse.py
@@ -5,10 +5,11 @@
 from __future__ import print_function, generators
 from os.path import exists
 from sys import stderr, stdin, stdout
+import re  # used to treat non-breaking spaces as spaces
 
 from lxml.etree import iterparse, XMLSyntaxError
 
-from utils import DAYS, save, load, army_time, parse_semester
+from utils import DAYS, save, army_time, parse_semester, ReturnSame, get_season
 from post import  get_calendar, get_bookstore
 
 def parse_catalog(html):
@@ -85,11 +86,23 @@ def parse_sections(html):
                 if ('course' in locals() and course != {}):  # this is old course
                     sections.append(course)
                 elem = elem.find('a')
-                course = {'section_link': base_url + elem.attrib.get('href', None)}
-                course['title'], course['UID'], tmp, course['section'] = elem.text.split(' - ')
+                link = elem.attrib.get('href', None)
+                if link.startswith('/'):
+                    link = base_url + link
+                course = {'section_link': link}
+                # some courses have '-' in title
+                if elem.text.count(' - ') > 3:
+                    a = elem.text.split(' - ')
+                    course['UID'], tmp, course['section'] = a[-3:]
+                    course['title'] = ' - '.join(a[:-3])
+                else:
+                    course['title'], course['UID'], tmp, course['section'] = elem.text.split(' - ')
                 course['abbr'], course['code'] = tmp.split(' ')
             elif elem.tag == 'span' and elem.attrib.get('class', None) == 'fieldlabeltext':
-                following = elem.tail.strip()
+                try:
+                    following = elem.tail.strip()
+                except AttributeError:
+                    continue
                 if elem.text == 'Associated Term: ':
                     course['semester'] = following
                 elif elem.text == 'Registration Dates: ':
@@ -97,16 +110,32 @@ def parse_sections(html):
                 elif elem.text == 'Levels: ':
                     course['level'] = following
                     elem = elem.getnext().getnext()
+                    if elem is None: continue
                     if elem.tag == 'span':
-                        course['attributes'] = elem.tail.strip()
-                        elem = elem.getnext().getnext()
-                    course['campus'] = elem.tail.strip().split(' Campus')[0]
+                        try:
+                            course['attributes'] = elem.tail.strip()
+                            elem = elem.getnext().getnext()
+                        except AttributeError:
+                            continue
+                    try:
+                        course['campus'] = elem.tail.strip().split(' Campus')[0]
+                    except AttributeError:
+                        continue
                     elem = elem.getnext()
-                    course['type'] = elem.tail.strip().split(' Schedule Type')[0]
+                    try:
+                        course['type'] = elem.tail.strip().split(' Schedule Type')[0]
+                    except AttributeError:
+                        continue
                     elem = elem.getnext()
-                    course['method'] = elem.tail.strip().split(' Instructional Method')[0]
+                    try:
+                        course['method'] = elem.tail.strip().split(' Instructional Method')[0]
+                    except AttributeError:
+                        continue
                     elem = elem.getnext()
-                    course['credits'] = int(elem.tail.strip().split('.000')[0])
+                    try:
+                        course['credits'] = int(elem.tail.strip().split('.000')[0])
+                    except AttributeError:
+                        continue
             elif elem.tag == 'a':
                 if elem.text == 'View Catalog Entry':
                     # TODO: get restrictions
@@ -114,22 +143,30 @@ def parse_sections(html):
                     # URL looks like this: base_url +
                     #     /BANP/bwckctlg.p_disp_course_detail?cat_term_in=201808&subj_code_in=ACCT&crse_numb_in=225
                     # can also follow and parse link below
-                    course['catalog_link'] = base_url + elem.attrib.get('href', None)
+                    link = elem.attrib.get('href', None)
+                    if link.startswith('/'):
+                        link = base_url + link
+                    course['catalog_link'] = link
                 elif elem.text == 'Bookstore':
                     # TODO: link directly to bookstore
                     # currently, looks like
                     # /BANP/bwckbook.site?p_term_in=201808&p_subj_in=ACCT&p_crse_numb_in=222&p_seq_in=001
                     # should be https://secure.bncollege.com/webapp/wcs/stores/servlet/TBListView
                     # requires POST
-                    course['bookstore_link'] = base_url + elem.attrib.get('href', None)
+                    link = elem.attrib.get('href', None)
+                    if link.startswith('/'):
+                        link = link + base_url
+                    course['bookstore_link'] = link
             elif elem.tag == 'table' and elem.attrib.get('class', None) == 'datadisplaytable' and elem.attrib.get('summary', None).endswith('this class..'):
-                for i, column in enumerate(elem.find('./tr[2]')):
+                elem = elem.getiterator('tr').__next__().getnext()
+                for i, column in enumerate(elem):
                     if i == 0:
-                        assert column.text == 'Class'
+                        if column.text != 'Class':
+                            print(column.text, file=stderr)
                     elif i == 1:
-                        if column.text is None:
-                            course['start_time'] = column.text
-                            course['end_time'] = column.text
+                        if column.text is None or column.text.strip() == '':
+                            course['start_time'] = 'TBA'
+                            course['end_time'] = 'TBA'
                         else:
                             course['start_time'], course['end_time'] = column.text.split(' - ')
                     elif i == 2:
@@ -152,49 +189,90 @@ def parse_sections(html):
                             if mail is not None:
                                 course['instructor_email'] = mail.attrib.get('href')
         except:
-            print(elem, elem.text, elem.tail, elem.attrib, course, elem.getchildren(), elem.getnext(), elem.getprevious().tail, file=stderr)
+            print(elem, elem.text, elem.tail, elem.attrib, course, elem.getchildren(),
+                  elem.getnext(), elem.getprevious().tail,
+                  elem.getparent().itertext().__next__(), file=stderr)
             if 'following' in locals():
                 print(following, file=stderr)
             if 'i' in locals() or 'column' in locals():
                 print(i, column, column.text, file=stderr)
             raise
+    sections.append(course)
     return sections
 
 
 def parse_days(text):
     text = text.split(' Meeting Times')[0]
-    if 'Only' in text:
+    if 'Session' in text:
+        return text  # don't mess with this
+    elif 'Only' in text:
         return DAYS[text.split(' Only')[0]]
     return ''.join(DAYS[d] for d in text.split('/'))
 
 
 def parse_exam(html):
-    return {parse_days(elem.text): {row.find('td').text.split(' - ')[1]:
+    d = {}
+    for _, elem in html:
+        if elem.tag == 'h5':
+            tmp = {}
+            try:
+                for row in elem.getparent().getnext().find('table').find('tbody').findall('tr'):
+                    meetings, time = row.findall('td')
+                    # TODO: THIS IS BAD
+                    if 'normal class meeting time' in time:  # Spring half-semester
+                        print('quitting')
+                        break
+                    if re.search(r'\s-\s', ''.join(meetings.itertext())) is not None:
+                        for meeting in re.split(r'\s-\s', ''.join(meetings.itertext()))[1].split(', '):
+                            tmp[meeting] = time
+                    else:
+                        assert any('all sections' in text.lower() for text in meetings.itertext())
+                        tmp = ReturnSame(time)
+                d[parse_days(elem.text)] = tmp
+            except:
+                print('tag:', elem.tag, 'text:', elem.text, 'row:', row)
+                if 'meetings' in locals():
+                    print('meetings:', list(meetings.itertext()), 'time:', time.text)
+                    if 'meeting' in locals():
+                        print('meeting:', meeting)
+                table = elem.getparent().getnext().find('table')
+                print(elem.getparent().getnext().attrib['class'],
+                      table.attrib['class'],
+                      list(table), 'row:', list(row), row[0].text)
+                raise
+    return d
+    '''return {parse_days(elem.text): {row.find('td').text.split(' - ')[1]:
                                     row.find('span').text
                                     for row in elem.getparent().getnext().find('table').find('tbody').findall('tr')}
-            for _, elem in html if elem.tag == 'h5'}
-
+            for _, elem in html if elem.tag == 'h5'}'''
 
 def parse_all_exams():
     result = {}
-    for year in range(13, 19):
-        for semester in ('spring', 'summer', 'fall'):
-            name = '20' + str(year) + parse_semester(semester)[-2:]
-            if not exists(name + '.html'):
-                save(get_calendar('20' + str(year), semester), name + '.html')
-            with open(name + '.html', 'rb') as stdin:
-                result[name] = parse_exam(iterparse(stdin, html=True))
+    for year in range(16, 19):
+        for semester in ('Spring', 'Summer', 'Fall'):
+            name = semester + '-' + '20' + str(year)
+            with open('exams/' + name + '.html', 'rb') as stdin:
+                try:
+                    result[name.replace('-', ' ')] = parse_exam(iterparse(stdin, html=True))
+                except:
+                    print(name)
+                    raise
     return result
 
 
 def clean_sections(sections):
-    if 'exams' not in globals():
-        exams = load('.exams.data')
+    #if 'exams' not in globals():
+    #    exams = load('.exams.data')
     for s in sections:
-        s['start_time'] = army_time(s['start_time'])
-        s['end_time'] = army_time(s['end_time'])
-        s['final_exam'] = exams[s['semester']][s['days']][s['start_time']]
-
+        try:
+            if 'start_time' in s:
+                s['start_time'] = army_time(s['start_time'])
+                s['end_time'] = army_time(s['end_time'])
+            #s['final_exam'] = exams[s['semester']][s['days']][s['start_time']]
+        except:
+            print(s, file=stderr)
+            raise
+    return sections
 
 def parse_b_and_n(html):
     '''
@@ -213,31 +291,27 @@ def parse_b_and_n(html):
     - required/recommended/optional
     '''
     books = []
+    for _, elem in html:
+        if elem.tag == 'div':
+            print(elem.attrib['class'])
 
+'''TODO: irrelevant
+Example: https://ssb.onecarolina.sc.edu/BANP/bwckbook.site?p_term_in=201808&p_subj_in=ACCT&p_crse_numb_in=222&p_seq_in=001
+        base_url = 'https://ssb.onecarolina.sc.edu/BANP/bwckbook.site'
+        redirect = "%s?p_term_in=%s&p_subj_in=%s&p_crse_numb_in=%s&p_seq_in=%s" % (base_url, semester, department, number, section)'''
 
 def get_books(semester, department, number, section):
-    '''Example: https://ssb.onecarolina.sc.edu/BANP/bwckbook.site?p_term_in=201808&p_subj_in=ACCT&p_crse_numb_in=222&p_seq_in=001'''
-    base_url = 'https://ssb.onecarolina.sc.edu/BANP/bwckbook.site'
-    redirect = "%s?p_term_in=%s&p_subj_in=%s&p_crse_numb_in=%s&p_seq_in=%s" % base_url, semester, department, number, section
-    return parse_b_and_n(get_bookstore(semester, department, number, section))
+    html = 'books/%s-%s-%s-%s-%s.html' % (get_season(semester), semester[:4],
+                                          department, number, section)
+    if not exists(html):
+        save(get_bookstore(semester, department, number, section), html, binary=False)
 
-
-def main(args):
-    if args.load:
-        print(load(args.output))
-    elif args.save or args.verbose:
-        if args.sections:
-            with open(args.input, 'rb') as stdin:
-                result = clean_sections(parse_sections(iterparse(stdin, html=True)))
-        elif args.catalog:
-            with open(args.input, 'rb') as stdin:
-                result = parse_catalog(iterparse(stdin, html=True))
-        else:
-            result = parse_all_exams()
-        if args.save:
-            save(result, args.output)
-        if args.verbose:
-            print(result)
+    course = parse_b_and_n(iterparse(open(html, 'rb'), html=True))
+    course['semester'] = semester
+    course['department'] = department
+    course['number'] = number
+    course['section'] = section
+    return course
 
 
 if __name__ == '__main__':
